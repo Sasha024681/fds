96df05180aa3a89222dc9dedcc8e557f38aafb9d
Sasha024681 <alexandraguzanov@gmail.com>
2026-01-18 21:09:25 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,69 @@
-import telebot 
-from config import token
+from random import randint
+import requests
 
-from logic import Pokemon
+class Pokemon:
+    pokemons = {}
+    # Инициализация объекта (конструктор)
+    def __init__(self, pokemon_trainer):
 
-bot = telebot.TeleBot(token) 
+        self.pokemon_trainer = pokemon_trainer   
 
-@bot.message_handler(commands=['go'])
-def go(message):
-    if message.from_user.username not in Pokemon.pokemons.keys():
-        pokemon = Pokemon(message.from_user.username)
-        bot.send_message(message.chat.id, pokemon.info())
-        bot.send_photo(message.chat.id, pokemon.show_img())
-    else:
-        bot.reply_to(message, "Ты уже создал себе покемона")
+        self.pokemon_number = randint(1,1000)
+        self.img = self.get_img()
+        self.name = self.get_name()
+        
+        self.hp = randint(25,50)
+        self.power = randint(1,3)
 
+        Pokemon.pokemons[pokemon_trainer] = self
 
-bot.infinity_polling(none_stop=True)
+    # Метод для получения картинки покемона через API
+    def get_img(self):
+        url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            return (data['sprites']['other']['official-artwork']["front_shiny"])
+        else:
+            return "https://img.freepik.com/premium-photo/girl-pikachu_551707-69798.jpg?semt=ais_hybrid&w=740"
+    
+    # Метод для получения имени покемона через API
+    def get_name(self):
+        url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            return (data['forms'][0]['name'])
+        else:
+            return "Pikachu"
+
+    def attack(self, enemy):
+        if isinstance(enemy, Wizard): # Проверка на то, что enemy является типом данных Wizard (является экземпляром класса Волшебник)
+            chance = randint(1,5)
+            if chance == 1:
+                return "Покемон-волшебник применил щит в сражении"
+        if enemy.hp > self.power:
+            enemy.hp -= self.power
+            return f"Сражение @{self.pokemon_trainer} с @{enemy.pokemon_trainer}"
+        else:
+            enemy.hp = 0
+            return f"Победа @{self.pokemon_trainer} над @{enemy.pokemon_trainer}! "
+
+    # Метод класса для получения информации
+    def info(self):
+        return f"Имя твоего покеомона: {self.name}, сила - {self.power}, здоровье{self.hp}"
+
+    # Метод класса для получения картинки покемона
+    def show_img(self):
+        return self.img
+
+class Wizard(Pokemon):
+    pass
+
+class Fighter (Pokemon):
+    def attack(self, enemy):
+        super_power = randint(5,10)
+        self.power += super_power
+        result = super().attack(enemy)
+        self.power -= super_power
+        return result + f"\nБоец применил супер-атаку силой:{super_power} "
